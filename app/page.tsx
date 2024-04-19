b7633b3478431b193a764dce127cc8a8efc41d72
MugishBeldar <mugishbeldar333@gmail.com>
2024-04-19 19:49:11 +0530

    Credential added for auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { register } from "@/instrumentation";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>main page</div>
-  );
+  useEffect(() => {
+    async function callOnce() {
+      await register();
+    }
+    callOnce();
+  }, []);
+  return <div>main page</div>;
 }
---
+++ instrumentation.ts
@@ -0,0 +1,25 @@
+"use server";
+import { db } from "./lib/db";
+
+export const register = async () => {
+  // if (process.env.NEXT_RUNTIME === "nodejs") {
+  // Check for admins
+  console.log("first");
+
+  const admin = await db.admin_user.count();
+  console.log("register ~ admin:", admin);
+  if (!admin) {
+    const data = await db.admin_user.create({
+      data: {
+        firstName: "admin",
+        lastName: "cryptomus",
+        email: "admin@gmail.com",
+        password: "admin@1234",
+        status: true,
+        roles: ["ADMIN"],
+      },
+    });
+    console.log("register ~ data:", data);
+  }
+};
+// };
