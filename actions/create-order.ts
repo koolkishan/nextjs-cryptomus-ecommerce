f1589acca572735b9a765b6f0d693f742fd767d7
MugishBeldar <mugishbeldar333@gmail.com>
2024-05-06 14:32:39 +0530

    fix callback url

--- actions/create-order.ts
+++ actions/create-order.ts
@@ -41,7 +41,7 @@ export async function createOrderAndOrderProducts(
           amount: amount.toString() + ".00",
           order_id: order.id.toString(),
           lifetime: 300,
-          url_callback: "https://3e66-165-225-120-150.ngrok-free.app/callback?id=" + order.id,  //  Add your ngrock webhook localhost not work 
+          url_callback: "https://2531-165-225-120-150.ngrok-free.app/callback?id=" + order.id,  //  Add your ngrock webhook localhost not work 
           url_return: `https://nextjs-cryptomus-ecommerce.vercel.app`,
           // url_return: `http://localhost:3000`,
           // url_callback: "https://1139-165-225-120-150.ngrok-free.app/callback?id=" + order.id, 
